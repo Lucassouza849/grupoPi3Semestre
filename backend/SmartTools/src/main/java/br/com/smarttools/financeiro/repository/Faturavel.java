37392eca74b868036a4064ad018d49f26d973802
PedroGimenezSilva <pedro.msilva@bandtec.com.br>
2021-10-15 18:10:16 -0300

    pagina quem somos conclída

--- backend/SmartTools/src/main/java/br/com/smarttools/financeiro/repository/Faturavel.java
+++ backend/SmartTools/src/main/java/br/com/smarttools/financeiro/repository/Faturavel.java
@@ -3,6 +3,7 @@ package br.com.smarttools.financeiro.repository;
 import br.com.smarttools.financeiro.model.Extrato;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface Faturavel extends JpaRepository<Extrato, Integer> {
+public interface Faturavel extends JpaRepository<Extrato, Integer>{
+
 
 }
