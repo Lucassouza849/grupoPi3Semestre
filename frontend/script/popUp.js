e46a05cc3a130440511d483d223fcdacabcd793a
Bruna Brito <bruna.melo@bandtec.com.br>
2021-11-17 18:45:45 -0300

    transacao concluida

--- frontend/script/popUp.js
+++ frontend/script/popUp.js
@@ -11,3 +11,37 @@ window.onload = function(){
 const btn_mais = document.querySelector('.btn_mais');
 btn_mais.addEventListener('click', () => iniciaModal('modal-side'));
 }
+
+function entrada_mostrar(){
+    entrada.style.display = 'block'
+    saida.style.display = 'none'
+}
+function saida_mostrar(){
+    entrada.style.display = 'none'
+    saida.style.display = 'block'
+}
+
+
+
+String.prototype.reverse = function(){
+    return this.split('').reverse().join(''); 
+  };
+  
+  function mascaraMoeda(campo,evento){
+    var tecla = (!evento) ? window.event.keyCode : evento.which;
+    var valor  =  campo.value.replace(/[^\d]+/gi,'').reverse();
+    var resultado  = "";
+    var mascara = "##.###.###,##".reverse();
+    for (var x=0, y=0; x<mascara.length && y<valor.length;) {
+      if (mascara.charAt(x) != '#') {
+        resultado += mascara.charAt(x);
+        x++;
+      } else {
+        resultado += valor.charAt(y);
+        y++;
+        x++;
+      }
+    }
+    campo.value = resultado.reverse();
+  }
+
